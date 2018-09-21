41961bfc65977f6212345daf86ee394f9a1e3b43
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-21 15:37:30 -0400

    add jwt auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: %i[create]
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'Failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :avatar, :first_name, :last_name)
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :username, :avatar, :first_name, :last_name
 end
