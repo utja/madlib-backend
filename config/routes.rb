d2f52aac20692fb6dbd5105365d81946fcddfcd1
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-24 14:16:30 -0400

    allow index only for templates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create]
-      resources :templates
+      resources :templates, only: %i[index]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
