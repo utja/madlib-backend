eebe9c6d941b562474765a995cc01147c0b8ff31
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-25 22:49:50 -0400

    add index create drawing params

--- app/controllers/api/v1/drawings_controller.rb
+++ app/controllers/api/v1/drawings_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::DrawingsController < ApplicationController
+
+  def index
+    drawings = Drawing.all
+    render json: drawings, status: :accepted
+  end
+
+  def create
+    drawing = Drawing.create(drawing_params)
+    if drawing.valid?
+      render json: drawing, status: :created
+    else
+      render json: { error: drawing.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
+  private
+  def drawing_params
+    params.require(:drawing).permit(:title, :data_url, :user_id, :story_id)
+  end
 end
