ab09872e2f556f7bbd53a322eb436fdbf3cde278
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-27 13:38:36 -0400

    fix true typo

--- db/migrate/20180926005743_create_drawings.rb
+++ db/migrate/20180926005743_create_drawings.rb
@@ -2,7 +2,7 @@ class CreateDrawings < ActiveRecord::Migration[5.2]
   def change
     create_table :drawings do |t|
       t.references :story, foreign_key: true
-      t.references :user, foreign_key: tru
+      t.references :user, foreign_key: true
       t.string :titlee
       t.text :data_url
 
