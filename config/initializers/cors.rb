d1e2da0ed426c31d8d8c7458a077a87825303446
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-11 18:13:01 -0400

    change cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:3001'
-    origins '*'
+    origins 'https://ad-scribitum.herokuapp.com/'
 
     resource '*',
       headers: :any,
